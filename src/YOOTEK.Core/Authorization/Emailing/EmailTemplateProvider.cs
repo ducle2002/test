896658ebd4229a23f6eb7fd1c2b5194b0f82725b
thuytran <slugtran1599@gmail.com>
2024-02-27 11:17:46 +0700

    template trungdo

--- src/YOOTEK.Core/Authorization/Emailing/EmailTemplateProvider.cs
+++ src/YOOTEK.Core/Authorization/Emailing/EmailTemplateProvider.cs
@@ -162,6 +162,15 @@ namespace Yootek.Emailing
                     return template;
                 }
             }
+             else if (tenantId == 47)
+            {
+                using (var stream = typeof(EmailTemplateProvider).GetAssembly().GetManifestResourceStream("YOOTEK.Authorization.Emailing.EmailTemplates.trungdo.html"))
+                {
+                    var bytes = stream.GetAllBytes();
+                    var template = Encoding.UTF8.GetString(bytes, 3, bytes.Length - 3);
+                    return template;
+                }
+            }
             else
             {
                 using (var stream = typeof(EmailTemplateProvider).GetAssembly().GetManifestResourceStream("YOOTEK.Authorization.Emailing.EmailTemplates.common_bill.html"))
